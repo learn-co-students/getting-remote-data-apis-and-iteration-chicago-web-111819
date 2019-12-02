a4852888d9b8f2ba45a7171c3fd96a5c84be3c46
kHuynhGitHub <huynh.h.kiimberly@gmail.com>
2019-12-02 17:06:45 -0600

    Done.

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,8 @@
 def welcome
-  # puts out a welcome message here!
+  puts "Welcome to the Star Wars API"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+    puts "Please enter a character name:"
+    name = gets.chomp.split.map(&:capitalize).join(' ')
 end
